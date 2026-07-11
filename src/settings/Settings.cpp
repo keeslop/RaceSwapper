fa4af3765e7b553ddd538dfa6283b34c8a7d1194
Keegan <>
2026-07-11 03:37:00 +0200

    clarification in ini comment

--- src/settings/Settings.cpp
+++ src/settings/Settings.cpp
@@ -24,7 +24,7 @@ void Settings::Load()
 
 	get_value(ini, Features::kDebugLogging, false, section, "bDebugLogging", "; Enable debug logging. WARNING: Debug logs are quite spammy when loading lots of NPCs");
 
-	get_value(ini, Features::kUseActorBase, false, section, "bUseActorBase", "; Use the ActorBase FormID instead of the Actor FormID for randomization.\n; This can help to keep the appearance of cloned NPCs stable.\n; Enable this if you use mods that clone NPCs, eg. YieldToMe, PAHE, Diary of Mine, etc.");
+	get_value(ini, Features::kUseActorBase, false, section, "bUseActorBase", "; Use the ActorBase FormID instead of the Actor FormID for randomization.\n; This can help to keep the appearance of cloned NPCs stable.\n; Enable this if you use mods that clone NPCs, eg. YieldToMe, PAHE, Diary of Mine, etc.\n; Enabling this reduces randomization variety somewhat.");
 
 	#ifdef _DEBUG 
 		Settings::GetSingleton()->features.set(Settings::Features::kDebugLogging);
